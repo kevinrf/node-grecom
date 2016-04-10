97c3b0a5a62cff97975ed44f13a0b231e6b7b0ad
Kevin Fuhst <kfuhst@gmail.com>
2016-04-09 18:40:49 -0600

    add message header/footer building code

--- grecom.js
+++ grecom.js
@@ -1,8 +1,18 @@
 "use strict";
+const STX = 2;
+const ETX = 3;
 
 class grecom {
   static about() {
     console.log("some cool stuff coming soon");
+  }
+
+  static pack(data) {
+    data.push(ETX);
+    var bytesum = data.reduce((x, y) => x + y);
+    data.push(bytesum);
+    data.unshift(STX);
+    return data;
   }
 }
 
