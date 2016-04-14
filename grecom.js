a9cf87bf1567cbf237ee5e74a522e697beb29152
Kevin Fuhst <kfuhst@gmail.com>
2016-04-13 23:47:14 -0600

    parse lcd state

--- grecom.js
+++ grecom.js
@@ -4,5 +4,6 @@ var constants = require("./lib/constants");
 var Grecom = { constants: constants };
 Grecom.Session = require("./lib/session");
 Grecom.Request = require("./lib/request");
+Grecom.Lcd = require("./lib/lcd");
 
 module.exports = Grecom;
