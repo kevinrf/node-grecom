b6c99bd6f91bf05e0f1ff44f58858ee08fef1f90
Kevin Fuhst <kfuhst@gmail.com>
2016-04-15 23:48:58 -0600

    dry session spec setup

--- spec/SessionSpec.js
+++ spec/SessionSpec.js
@@ -4,43 +4,44 @@ var Session = require("../lib/session");
 var Request = require("../lib/request")
 var constants = require("../lib/constants");
 
-describe("#write", () => {
-  let session = new Session("/dev/tty.test", {connect: false});
-
-  it("sends request data to the serial port", () => {
-    let request = new Request([7,8,9]);
-    spyOn(session._port, 'write');
-    session.write(request);
-    expect(session._port.write).toHaveBeenCalledWith(request.bytes);
-  });
-});
-
-describe ("#keyPress", () => {
-  let session = new Session("/dev/tty.test", {connect: false});
-
-  it ("writes a request with the send key command byte", () => {
-    spyOn(session, 'write');
-    session.sendKey(constants.key.LIGHT);
-    expect(session.write).toHaveBeenCalled();
-    let request = session.write.calls.argsFor(0)[0];
-    expect(request.command).toEqual(constants.command.SEND_KEY);
+describe("Session", () => {
+  var session;
+  beforeEach(() => {
+    session = new Session("/dev/tty.test", {connect: false});
   });
 
-  it ("writes a request with the key value as payload", () => {
-    spyOn(session, 'write');
-    let key = constants.key.LIGHT;
-    session.sendKey(key);
-    expect(session.write).toHaveBeenCalled();
-    let request = session.write.calls.argsFor(0)[0];
-    expect(request.messageData).toEqual([key]);
+  describe("#write", () => {
+    it("sends request data to the serial port", () => {
+      let request = new Request([7,8,9]);
+      spyOn(session._port, 'write');
+      session.write(request);
+      expect(session._port.write).toHaveBeenCalledWith(request.bytes);
+    });
   });
-});
 
-describe ("#queueForResponse", () => {
-  let session = new Session("/dev/tty.test", {connect: false});
+  describe ("#sendKey", () => {
+    it ("writes a request with the send key command byte", () => {
+      spyOn(session, 'write');
+      session.sendKey(constants.key.LIGHT);
+      expect(session.write).toHaveBeenCalled();
+      let request = session.write.calls.argsFor(0)[0];
+      expect(request.command).toEqual(constants.command.SEND_KEY);
+    });
 
-  it ("executes the callback when a matching response is received", (done) => {
-    session.queueForResponse(10, (bytes) => { done() });
-    session._handleResponse(new Buffer([2, 10, 5, 5, 3, 23]));
+    it ("writes a request with the key value as payload", () => {
+      spyOn(session, 'write');
+      let key = constants.key.LIGHT;
+      session.sendKey(key);
+      expect(session.write).toHaveBeenCalled();
+      let request = session.write.calls.argsFor(0)[0];
+      expect(request.messageData).toEqual([key]);
+    });
+  });
+
+  describe ("#queueForResponse", () => {
+    it ("executes the callback when a matching response is received", (done) => {
+      session.queueForResponse(10, (bytes) => { done() });
+      session._handleResponse(new Buffer([2, 10, 5, 5, 3, 23]));
+    });
   });
 });
