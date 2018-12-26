0c3ad17efe515c965b841c00ceead66de689ea09
Kevin Fuhst <kfuhst@gmail.com>
2018-12-26 07:32:08 -0700

    add basic typescript configuration

--- grecom.js
+++ grecom.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var constants = require("./lib/constants");
+var constants = require("./src/constants");
 var serialPort = require("serialport");
 var Grecom = {constants: constants};
-Grecom.Session = require("./lib/session");
+Grecom.Session = require("./src/session");
 
 Grecom.connect = path => {
   let port = new serialPort.SerialPort(path, {baudrate: 115200});
