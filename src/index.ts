d4870d73224b00fa7680a4ebc520fc337201c630
Kevin Fuhst <kfuhst@gmail.com>
2019-04-03 11:09:40 -0600

    add factory method for sessions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export {default as SerialPortFacade} from "./serialPortFacade";
+import * as SerialPort from "serialport";
+import SerialPortFacade from "./serialPortFacade";
+import Session from "./session";
+
+export function connect(path: string): Session {
+  const port = new SerialPort(path, {baudRate: 115200});
+  const serialPortFacade = new SerialPortFacade(port);
+  return new Session(serialPortFacade);
+}
+
 export {default as Session} from "./session";
-export {default as constants} from "./constants";
+export {default as Constants} from "./constants";
